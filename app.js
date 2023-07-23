6ff011e4194eab6b1cac2023649149879e5b95eb
CS-parth <parth.vijy@gmail.com>
2023-07-23 12:03:04 +0530

    added database connectivity

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 var app = express();
 
 app.set("view engine","ejs");
 
+app.use(express.static('res'));
 app.use(express.urlencoded({extended: true}));
 
-var items = [];
-var value = "new task";
-app.get("/",(req,res)=>{
-    res.render('list',{list: items});
+
+mongoose.connect("mongodb://127.0.0.1:27017/Todo");
+
+const trySchema = new mongoose.Schema({
+    name: String
+})
+
+var items = mongoose.model("Tasks",trySchema); // ans items array having elemet name task and each element having Schema 
+// Tasks is a collection which can be accssed thorugh items
+
+app.get("/",async (req,res)=>{
+    const data = await items.find();
+    res.render('list',{list: data});
 })
 
 app.post("/",(req,res)=>{
     var item = req.body.task;
-    items.push(item);
+    var task = new items({
+        name: req.body.task
+    });
+    task.save();
     res.redirect('/');
 })
 
